5ec4fb0c66fd3c17e82227cc91f2156f93416fef
Heshie Brody <4hbrody@gmail.com>
2018-08-28 02:29:02 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,13 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(file_path)
     self.path = file_path
+    files = [ ]
+  end
+
+  def files
+
   end
 
 end
