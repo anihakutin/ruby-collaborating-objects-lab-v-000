7b2d70bd469bef202d364449b52a777f56588020
Heshie Brody <4hbrody@gmail.com>
2018-08-28 03:00:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
 
   def self.new_by_filename(file_name)
     song = self.new(file_name.split("-")[1].strip)
-    song.artist = artist_name(file_name.split("-")[0].strip)
+    song.artist = self.artist_name(file_name.split("-")[0].strip)
   end
   def artist_name=(artist_name)
     if (self.artist.nil?)
