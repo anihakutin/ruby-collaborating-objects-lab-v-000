a6dc91c8ff0fba0f82502a64fba93c2afcc122d0
Heshie Brody <4hbrody@gmail.com>
2018-08-28 02:48:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
     self.name = name
   end
 
-  def self.new_by_filename(name)
+  def self.new_by_filename(file_name)
     song = self.new
-    song.name = name
+    song.name = file_name
   end
 end
