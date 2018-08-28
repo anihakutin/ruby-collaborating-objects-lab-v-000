ec3827af126c0778c273eacc645ae07222a118ec
Heshie Brody <4hbrody@gmail.com>
2018-08-28 02:54:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,6 @@ class Song
 
   def self.new_by_filename(file_name)
     song = self.new(file_name.split("-")[1].strip)
-    song.artist = 
+    song.artist = file_name.split("-")[0].strip
   end
 end
